31ee21038f1e6fcd9e36ff019b02f654b0928600
Maksym Ivanov <maksym.ivanov057@gmail.com>
2023-03-26 19:29:43 +0200

    Full Docker automation

--- run_script.sh
+++ run_script.sh
@@ -7,22 +7,32 @@
 echo -e "\nStarting script ...\n"
 
 echo -e "\nInitializing Terraform ...\n"
-# terraform init
+terraform init
 
 echo -e "\nApplying Terraform ...\n"
 terraform apply -auto-approve | tee output.txt
 
-echo -e "\nParsing Instance DNS name ...\n"
+echo -e "\nParsing Instance DNS name and public IP ...\n"
 publicdns=$(grep 'instance_dns_name = "' output.txt | cut -b 26-)
 public_dns_name=${publicdns%?}
+publicip=$(grep 'instance_public_ip = "' output.txt | cut -b 23-)
+public_ip=${publicip%?}
 
+# Wait some time for fully instance setup to execute scp
 echo -e "\nWaiting for scp command to execute ...\n"
-sleep 8
+sleep 12
+
+echo -e "\nCopying Docker folder...\n"
 scp -r -i ~/dev_folder/test_task_tf/tf_key.pem ~/dev_folder/test_task_tf/docker_dir ubuntu@$public_dns_name:~/
 
-# echo -e "\nNow copy Docker folder to created server:"
-# echo -e 'scp -r -i "tf_key.pem" ~/dev_folder/test_task_tf/docker_dir ubuntu@'$public_dns_name':~/\n'
+echo -e "\nWaiting for docker-compose up to execute ...\n"
+sleep 6
 
-# echo -e "\nYou can connect to created instance via SSH:"
-# echo 'ssh -i "tf_key.pem" ubuntu@<public dns name above>'
+echo -e "\nExecuting Docker-compose on created instance\n"
+ssh -i "tf_key.pem" ubuntu@$public_dns_name "echo 'ubuntu ALL=(ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers"
+sleep 6
+ssh -i "tf_key.pem" ubuntu@$public_dns_name "cd ~/docker_dir && sudo /snap/bin/docker-compose up -d"
 
+sleep 3
+echo -e "\nNow you can reach WordPress start page via:"
+echo $public_ip
